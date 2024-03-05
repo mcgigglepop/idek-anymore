4cfcd81a1b93d67e11fbf981d133bb3ae0c53d98
cristopher <christoph.alano@gmail.com>
2024-03-05 15:35:20 -0600

    change

--- MyGymHero/src/components/commonComponents/ColorPicker.js
+++ MyGymHero/src/components/commonComponents/ColorPicker.js
@@ -1,74 +1,83 @@
-import React, { useState, useMemo } from "react";
-import { View, Text, Image, Modal, TouchableOpacity } from 'react-native';
-import ColorPicker from 'react-native-wheel-color-picker';
-import { ColorpickerStyle } from '../../styles';
-import { SW, SF } from '../../utils';
-import { color_picker_set_action } from "../../redux/action/CommonAction";
-import { useDispatch } from "react-redux";
-import { Button } from '../../components';
-import { RouteName } from '../../routes';
-import images from '../../index';
-import { useNavigation, useTheme } from '@react-navigation/native';
-import { useTranslation } from "react-i18next";
+import React, {useState, useMemo} from 'react';
+import {Text} from 'react-native';
+// import {View, Text, Image, Modal, TouchableOpacity} from 'react-native';
+// import ColorPicker from 'react-native-wheel-color-picker';
+// import {ColorpickerStyle} from '../../styles';
+// import {SW, SF} from '../../utils';
+// import {color_picker_set_action} from '../../redux/action/CommonAction';
+// import {useDispatch} from 'react-redux';
+// import {Button} from '../../components';
+// import {RouteName} from '../../routes';
+// import images from '../../index';
+// import {useNavigation, useTheme} from '@react-navigation/native';
+// import {useTranslation} from 'react-i18next';
 
-const ColorPickerset = (props) => {
-  const { Colors } = useTheme();
-  const { t } = useTranslation();
-  const ColorPickerStyles = useMemo(() => ColorpickerStyle(Colors), [Colors]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [currentColor, setCurrentColor] = useState('');
-  const dispatch = useDispatch();
-  const onColorChange = (selectedColor) => {
-    setCurrentColor(selectedColor);
-    dispatch(color_picker_set_action(selectedColor))
-  };
-  const navigation = useNavigation();
+const ColorPickerset = props => {
+  // const {Colors} = useTheme();
+  // const {t} = useTranslation();
+  // const ColorPickerStyles = useMemo(() => ColorpickerStyle(Colors), [Colors]);
+  // const [modalVisible, setModalVisible] = useState(false);
+  // const [currentColor, setCurrentColor] = useState('');
+  // const dispatch = useDispatch();
+  // const onColorChange = selectedColor => {
+  //   setCurrentColor(selectedColor);
+  //   dispatch(color_picker_set_action(selectedColor));
+  // };
+  // const navigation = useNavigation();
   return (
-    <View>
-      <View style={ColorPickerStyles.CenteredViewtwo}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-        >
-          <View style={ColorPickerStyles.CenteredView}>
-            <View style={ColorPickerStyles.ModalView}>
-              <View style={ColorPickerStyles.SetHeight}>
-                <View
-                  style={[
-                    { backgroundColor: currentColor, borderRadius: SW(7) },
-                  ]}
-                >
-                  <Text style={ColorPickerStyles.setcolorwhite}>{currentColor}</Text>
-                  <ColorPicker
-                    color={currentColor}
-                    onColorChange={onColorChange}
-                    onColorSelected={Colors.red}
-                    thumbSize={50}
-                    noSnap={true}
-                    defaultProps={true}
-                    row={false}
-                    gapSize={0}
-                    discreteLength={0}
-                    sliderHidden={true}
-                    discrete={true}
-                  />
-                </View>
-              </View>
-              <View style={ColorPickerStyles.setbuttonwidth}>
-                <Button title={t("Ok")}
-                  buttonStyle={{ backgroundColor: Colors.theme_background }}
-                  onPress={() => { setModalVisible(false); navigation.replace(RouteName.HOME_SCREEN) }}
-                />
-              </View>
-            </View>
-          </View>
-        </Modal>
-        <TouchableOpacity style={ColorPickerStyles.PaddingRight} onPress={() => setModalVisible(true)}>
-          <Image style={ColorPickerStyles.colorpickerpickerimagwidth} resizeMode='cover' source={images.Color_picker_image} />
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
-}
+    <Text>Test</Text>
+    // <View>
+    //   <View style={ColorPickerStyles.CenteredViewtwo}>
+    //     <Modal animationType="slide" transparent={true} visible={modalVisible}>
+    //       <View style={ColorPickerStyles.CenteredView}>
+    //         <View style={ColorPickerStyles.ModalView}>
+    //           <View style={ColorPickerStyles.SetHeight}>
+    //             <View
+    //               style={[
+    //                 {backgroundColor: currentColor, borderRadius: SW(7)},
+    //               ]}>
+    //               <Text style={ColorPickerStyles.setcolorwhite}>
+    //                 {currentColor}
+    //               </Text>
+    //               <ColorPicker
+    //                 color={currentColor}
+    //                 onColorChange={onColorChange}
+    //                 onColorSelected={Colors.red}
+    //                 thumbSize={50}
+    //                 noSnap={true}
+    //                 defaultProps={true}
+    //                 row={false}
+    //                 gapSize={0}
+    //                 discreteLength={0}
+    //                 sliderHidden={true}
+    //                 discrete={true}
+    //               />
+    //             </View>
+    //           </View>
+    //           <View style={ColorPickerStyles.setbuttonwidth}>
+    //             <Button
+    //               title={t('Ok')}
+    //               buttonStyle={{backgroundColor: Colors.theme_background}}
+    //               onPress={() => {
+    //                 setModalVisible(false);
+    //                 navigation.replace(RouteName.HOME_SCREEN);
+    //               }}
+    //             />
+    //           </View>
+    //         </View>
+    //       </View>
+    //     </Modal>
+    //     <TouchableOpacity
+    //       style={ColorPickerStyles.PaddingRight}
+    //       onPress={() => setModalVisible(true)}>
+    //       <Image
+    //         style={ColorPickerStyles.colorpickerpickerimagwidth}
+    //         resizeMode="cover"
+    //         source={images.Color_picker_image}
+    //       />
+    //     </TouchableOpacity>
+    //   </View>
+    // </View>
+  );
+};
 export default ColorPickerset;
