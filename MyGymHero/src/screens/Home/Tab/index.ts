2d5e8bd6cfde44e7c547bfa46e9a01caf21672f6
cristopher <christoph.alano@gmail.com>
2024-03-05 16:25:56 -0600

    tab files

---
+++ MyGymHero/src/screens/Home/Tab/index.ts
@@ -0,0 +1,4 @@
+export {default as HomeTab} from './HomeTab';
+export {default as TrainingTab} from './TrainingTab';
+export {default as MydiaryTab} from './MydiaryTab';
+export {default as Profile} from './Profile';
